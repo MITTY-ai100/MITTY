cb1fc3d81b3108be5f07427a4c94454529411b74
KoJaem <woalswhwh@naver.com>
2024-06-15 10:00:32 +0900

    initial settings

---
+++ test.py
@@ -0,0 +1,17 @@
+# start get env region
+
+# from dotenv import load_dotenv
+# import os 
+
+# # load .env
+# load_dotenv()
+
+# openAPIKey = os.environ.get('OPEN_API_KEY')
+
+# print(openAPIKey)
+
+# end get env region
+
+title = "텔레토비"
+
+print(title)
